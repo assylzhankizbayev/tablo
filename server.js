7c06e667ea04c12cce930312522f0dd1e0475989
assylzhan <assylzhan.kizbayev@gmail.com>
2021-02-21 18:45:18 +0600

    server changes

--- server.js
+++ server.js
@@ -1,20 +1,27 @@
-const httpServer = require("http").createServer(handler);
+const httpServer = require("http").createServer();
 const io = require("socket.io")(httpServer, {
   cors: { origin: "*" },
 });
 const fs = require("fs");
 const path = require("path");
 const PORT = 8080;
-
-let id = 0;
+let regIdx = 0;
+let calIdx = 0;
 
 io.on("connection", (socket) => {
   console.log(`Connected User, ID: ${socket.id}`);
+  socket.emit('init', { regIdx, calIdx })
 
-  socket.on("queue", (queue) => {
-    id++;
-    console.log('New queue from user', id);
-    socket.broadcast.emit("userInfo", { id, room: 101 });
+  socket.on("registration", (queue) => {
+    regIdx++;
+    console.log('Registered new user', regIdx);
+    socket.broadcast.emit("registered-users-amount", { amount: regIdx });
+  });
+
+  socket.on("call-next-user", (queue) => {
+    calIdx++;
+    console.log('Call user', calIdx);
+    socket.broadcast.emit("user-info", { id: calIdx });
   });
 });
 

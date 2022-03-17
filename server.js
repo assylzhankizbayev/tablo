b7be9b70c9c02806bcbdf6756a889ba77f33dd1b
Assylzhan Kizbayev <AKizbayev@one.kz>
2022-03-17 11:42:29 +0600

    app changes

--- server.js
+++ server.js
@@ -1,31 +1,35 @@
-const httpServer = require("http").createServer();
+const express = require("express");
+const app = express();
+const httpServer = require("http").Server(app);
 const io = require("socket.io")(httpServer, {
   cors: { origin: "*" },
 });
 const PORT = 8080;
+
+app.get("/", (req, res) => {
+  res.json("OK");
+});
+
 let regIdx = 0;
 let calIdx = 0;
 
 io.on("connection", (socket) => {
   console.log(`Connected User, ID: ${socket.id}`);
-  socket.emit('init', { regIdx, calIdx })
+  socket.emit("init", { regIdx, calIdx });
 
   socket.on("registration", (queue) => {
     regIdx++;
-    console.log('Registered new user', regIdx);
+    console.log("Registered new user", regIdx);
     socket.broadcast.emit("registered-users-amount", { amount: regIdx });
   });
 
   socket.on("call-next-user", (queue) => {
     calIdx++;
-    console.log('Call user', calIdx);
+    console.log("Call user", calIdx);
     socket.broadcast.emit("user-info", { id: calIdx });
   });
 });
 
-
 httpServer.listen(PORT, () =>
   console.log(`listening on http://localhost:${PORT}`)
 );
-
-
